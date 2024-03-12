e1d8dd5f75abdbae4aff5c6a99aba7d8eae9c3cf
p <i@yangchnx.com>
2024-03-12 03:59:53 +0000

    add green options

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,10 @@ import "../styles.scss";
 import {
     Box,
     Button,
+    Checkbox,
     ClickAwayListener,
     Container,
+    FormControlLabel,
     Link as MuiLink,
     TextField,
     Tooltip,
@@ -18,25 +20,28 @@ import { pinyin } from 'pinyin-pro';
 import ToJyutping from "to-jyutping";
 
 import { getLocaleText, I18nText } from "../utils/I18n";
-import { mancan_convert } from "../utils/mancan_convert";
+import { mancan_convert, mancan_options } from "../utils/mancan_convert";
 
 interface Pronunciation {
     hanzi: string;
     py_initial: string;
     py_final: string;
-    py_tone: number;
+    py_tone: string;
     jp_initial: string;
     jp_final: string;
-    jp_tone: number;
+    jp_tone: string;
     my_initial: string;
     my_final: string;
-    my_tone: number;
+    my_tone: string;
 }
 
 const Highlight = styled("span")(({ theme }) => ({
     color: theme.palette.error.main,
 }));
 
+const GreenHighlight = styled("span")(({ theme }) => ({
+    color: theme.palette.success.main,
+}));
 
 function mergeArrays(array1: [string, string | null][], array2: string[]): string[][] {
     const mergedArray: string[][] = [];
@@ -61,10 +66,11 @@ export default function Home(props: { lang: keyof I18nText }) {
 
     const [input, setInput] = React.useState<string>("廣州話");
     const [result, setResult] = React.useState<Pronunciation[]>([]);
-    
+    const [selectedOptions, setSelectedOptions] = React.useState<string[]>([]);
+
     const hanjppy_add_my = (hanjppy: string[]): Pronunciation => {
         let [hanzi, jp, py] = hanjppy;
-    
+
         // 拆解 jyut ping
         let jp_tone = 0;
         if (!isNaN(parseInt(jp.charAt(jp.length - 1)))) {
@@ -78,16 +84,16 @@ export default function Home(props: { lang: keyof I18nText }) {
             i++;
         }
         let jp_final = jp.slice(i);
-    
+
         // 拆解 pinyin
         let py_origin = py;
         py = py.replace(/yi/g, 'i');
         py = py.replace(/y/g, 'i');
         py = py.replace(/wu/g, 'u');
         py = py.replace(/ü/g, 'yu');
-        let py_tone = 0;
+        let py_tone = "0";
         if (!isNaN(parseInt(py.charAt(py.length - 1)))) {
-            py_tone = parseInt(py.charAt(py.length - 1));
+            py_tone = py.charAt(py.length - 1);
             py = py.slice(0, -1);
         }
         let py_initial = "";
@@ -97,38 +103,39 @@ export default function Home(props: { lang: keyof I18nText }) {
             i++;
         }
         let py_final = py.slice(i);
-    
-        let [my_initial, my_final, my_tone] = mancan_convert(py_origin, py_initial, py_final, py_tone);
-    
+
+        let [my_initial, my_final, my_tone] = mancan_convert(
+            py_origin, py_initial, py_final, py_tone, selectedOptions, jp_final
+        );
+
         return {
             hanzi: hanzi,
             jp_initial: jp_initial,
             jp_final: jp_final,
-            jp_tone: jp_tone,
+            jp_tone: "" + jp_tone,
             py_initial: py_initial,
             py_final: py_final,
-            py_tone: py_tone,
+            py_tone: "" + py_tone,
             my_initial: my_initial,
             my_final: my_final,
-            my_tone: my_tone
+            my_tone: "" + my_tone
         };
     }
-    
+
     function translateMandarinToCantonese(input: string): Pronunciation[] {
         let jps = ToJyutping.getJyutpingList(input);
-    
         let pys = pinyin(input, {
             toneType: 'num',              // 启用多音字模式
             type: 'array',                // 启用分词，以解决多音字问题。默认不开启，使用 true 开启使用 Intl.Segmenter 分词库。
         })
-    
+
         // [[hanzi, jp, py]...]
         let mergedArray = mergeArrays(jps, pys);
-    
+
         console.log(jps)
         console.log(pys)
         console.log(mergedArray)
-    
+
         // 模拟转换逻辑，返回示例数据
         return mergedArray.map(hanjppy_add_my);
     }
@@ -153,9 +160,26 @@ export default function Home(props: { lang: keyof I18nText }) {
         setOpenTooltipIndex(openTooltipIndex === index ? null : index);
     };
 
+    // 处理单个选项的点击
+    const handleOptionChange = (value: string) => {
+        setSelectedOptions((prev) =>
+            prev.includes(value) ? prev.filter((option) => option !== value) : [...prev, value],
+        );
+    };
+
+    // 全选
+    const handleSelectAll = () => {
+        setSelectedOptions(mancan_options.map((option) => option.value));
+    };
+
+    // 全不选
+    const handleDeselectAll = () => {
+        setSelectedOptions([]);
+    };
+
     return (
         <Container maxWidth="md">
-            <Typography variant="h5">
+            <Typography variant="h3" m={2}>
                 {getLocaleText(
                     {
                         "zh-Hans": "普通粤",
@@ -177,20 +201,66 @@ export default function Home(props: { lang: keyof I18nText }) {
                 )}
             </Typography>
 
-            <Typography variant="body1">
+            <Typography variant="body1" m={1}>
                 {getLocaleText(
                     {
-                        "zh-Hans": "目前使用最简单的推导规则，并标出所有不合此规则之要素。",
+                        "zh-Hans": "使用通用而简单的推导规则，直接用普通话读音推到广州话读音，并标出所有不合此规则之要素。",
                     },
                     lang
                 )}
             </Typography>
-            <Typography variant="body1">
+            <Typography variant="body1" m={1}>
+                {getLocaleText(
+                    {
+                        "zh-Hans": "符合以下最简单一对一规则者，该部分拼音显示为黑色（暗色模式为白色）。",
+                    },
+                    lang
+                )}
+            </Typography>
+            <Typography variant="body1" m={1}>
 
                 <MuiLink href="https://github.com/pustot/PustoNoto/blob/master/400-Lingvo/400-zh-yue-%E7%B2%B5-Kantona.md#%E6%99%AE%E8%BD%89%E7%B2%B5%E8%BD%AC%E6%8D%A2%E8%A7%84%E5%88%99%E4%B8%8E%E4%B8%8D%E8%A7%84%E5%88%99%E6%80%BB%E7%BB%93"
                     target="_blank" rel="noopener noreferrer">
                     规则说明
                 </MuiLink>
+            </Typography>
+
+            <Typography variant="body1" m={1}>
+                {getLocaleText(
+                    {
+                        "zh-Hans": "符合以下复选框中选中的比较通用的规则者，该部分拼音显示为绿色。",
+                    },
+                    lang
+                )}
+            </Typography>
+
+            {/* 显示选项 */}
+            <Box m={2}>
+                {mancan_options.map((option) => (
+                    <FormControlLabel
+                        key={option.value}
+                        control={
+                            <Checkbox
+                                checked={selectedOptions.includes(option.value)}
+                                onChange={() => handleOptionChange(option.value)}
+                            />
+                        }
+                        label={option.label}
+                    />
+                ))}
+                <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                    <Button variant="contained" onClick={handleSelectAll} sx={{ mr: 2 }}>全选</Button>
+                    <Button variant="contained" onClick={handleDeselectAll}>全不选</Button>
+                </Box>
+            </Box>
+
+            <Typography variant="body1" m={1}>
+                {getLocaleText(
+                    {
+                        "zh-Hans": "不符合上述规则者，该部分拼音显示为红色。",
+                    },
+                    lang
+                )}
             </Typography>
 
 
@@ -225,12 +295,24 @@ export default function Home(props: { lang: keyof I18nText }) {
                                     onMouseLeave={handleTooltipClose}
                                     onClick={() => handleTooltipOpen(index)}
                                 >
-                                    <Typography sx={{ fontSize: '0.75em', lineHeight: '1' }}>
-                                        {pronunciation.jp_initial !== pronunciation.my_initial ? <Highlight>{pronunciation.jp_initial}</Highlight> : pronunciation.jp_initial}
-                                        {pronunciation.jp_final !== pronunciation.my_final ? <Highlight>{pronunciation.jp_final}</Highlight> : pronunciation.jp_final}
-                                        {pronunciation.jp_tone !== pronunciation.my_tone ? <Highlight>{pronunciation.jp_tone}</Highlight> : pronunciation.jp_tone}
+                                    <Typography sx={{ fontSize: '1em', lineHeight: '1' }}>
+                                        {pronunciation.jp_initial !== pronunciation.my_initial ?
+                                            (pronunciation.my_initial.split('/').some(part => part === pronunciation.jp_initial) ?
+                                                <GreenHighlight>{pronunciation.jp_initial}</GreenHighlight> :
+                                                <Highlight>{pronunciation.jp_initial}</Highlight>) :
+                                            pronunciation.jp_initial}
+                                        {pronunciation.jp_final !== pronunciation.my_final ?
+                                            (pronunciation.my_final.split('/').some(part => part === pronunciation.jp_final) ?
+                                                <GreenHighlight>{pronunciation.jp_final}</GreenHighlight> :
+                                                <Highlight>{pronunciation.jp_final}</Highlight>) :
+                                            pronunciation.jp_final}
+                                        {pronunciation.jp_tone !== pronunciation.my_tone ?
+                                            (pronunciation.my_tone.split('/').some(part => part === pronunciation.jp_tone) ?
+                                                <GreenHighlight>{pronunciation.jp_tone}</GreenHighlight> :
+                                                <Highlight>{pronunciation.jp_tone}</Highlight>) :
+                                            pronunciation.jp_tone}
                                     </Typography>
-                                    <Typography sx={{ lineHeight: '1.5' }}>{pronunciation.hanzi}</Typography>
+                                    <Typography sx={{ lineHeight: '1.5' }} variant="h6">{pronunciation.hanzi}</Typography>
                                 </Box>
                             </Tooltip>
                         ))}
--- src/utils/mancan_convert.ts
+++ src/utils/mancan_convert.ts
@@ -3,7 +3,7 @@ const simple_mapper_initial = new Map([
     ["b", "b"], ["p", "p"], ["m", "m"], ["f", "f"],
     ["d", "d"], ["t", "t"], ["n", "n"], ["l", "l"],
     ["g", "g"], ["k", "k"], ["h", "h"],
-    ["j", ""], ["q", ""], ["x", ""],
+    // ["j", ""], ["q", ""], ["x", ""],
     ["zh", "z"], ["ch", "c"], ["sh", "s"], ["r", "j"],
     ["z", "z"], ["c", "c"], ["s", "s"],
 ]);
@@ -20,24 +20,59 @@ const simple_mapper_final = new Map([
     ["iang", "oeng"], ["uang", "ong"],
 ]);
 
+const jt_mapper = new Map([
+    ["j", "z/g"], ["q", "c/k"], ["x", "s/h"],
+]);
+
+// 定义转换选项
+export const mancan_options = [
+    { label: '尖团音', value: 'jt' },
+    { label: '闭口韵（咸深）', value: 'bk' },
+    { label: '阴阳上阴阳去', value: 'yy' },
+    // 可以根据需要添加更多选项
+    { label: '入声字', value: 'rs' },
+];
+
 export const mancan_convert = (
     py: string,
     py_initial: string,
     py_final: string,
-    py_tone: number
-): [string, string, number] => {
+    py_tone: string,
+    selectedOptions: string[],
+    jp_final: string
+): [string, string, string] => {
     // simple mapping
     let my_initial = simple_mapper_initial.get(py_initial) || "";
     let my_final = simple_mapper_final.get(py_final) || "";
-    let my_tone = 0;
-    if (py_tone == 1) my_tone = 1;
-    else if (py_tone == 2) my_tone = 4;
+    let my_tone = "0";
+    if (py_tone == "1") my_tone = "1";
+    else if (py_tone == "2") my_tone = "4";
     // simple mapping: for beginning y and w and gu ku
     if (my_initial == "" && py[0] == "y") my_initial = "j";
     if (my_initial == "" && py[0] == "w" && py.substring(0, 4) !== "weng") my_initial = "w";
-    if (my_initial === "g" && py.substring(0, 2) === "gu" ) my_initial = "gw";
-    if (my_initial === "k" && py.substring(0, 2) === "ku" ) my_initial = "kw";
+    if (my_initial === "g" && py.substring(0, 2) === "gu") my_initial = "gw";
+    if (my_initial === "k" && py.substring(0, 2) === "ku") my_initial = "kw";
 
+    // Options
 
+    // 最基本的逻辑，放最下面
+    // 尖团音
+    if (selectedOptions.includes('jt') && !my_initial) {
+        my_initial = jt_mapper.get(py_initial) || my_initial;
+    }
+    // 闭口韵（咸深）
+    if (selectedOptions.includes('bk') && (my_final.endsWith("m") || my_final.endsWith("n"))) {
+        my_final = my_final.slice(0, -1) + "m/" + my_final.slice(0, -1) + "n";
+    }
+    // 入声字
+    if (selectedOptions.includes('rs') && /[ptk]$/.test(jp_final)) {
+        my_final = "/" + jp_final;
+    }
+    // 阴阳上阴阳去
+    if (selectedOptions.includes('yy') && (py_tone == "3" || py_tone == "4")) {
+        if (py_tone == "3") my_tone = "2/5";
+        if (py_tone == "4") my_tone = "3/6";
+    }
+    console.log(jt_mapper.get(py_initial))
     return [my_initial, my_final, my_tone];
 }
